11a9ccdd80ead7057f206498e91fbe5a497f1eca
Hazari PraveenKumar <phazari@codeaurora.org>
2019-03-16 15:51:19 +0100

    raphael: Disable VSync for CPU rendered apps
    Choreographer VSync has some performance impact for CPU rendered apps
    as the app has to wait for VSync to start a new frame draw. When the
    frame draw time exceeds one VSync, this has a direct impact on the FPS.
    "debug.cpurend.vsync" property controls the VSync for CPU rendered apps.
    
    Disable VSync for CPU rendered app, by setting this property to false.
    
    Change-Id: I93cfa7c588f1ab80803940c8059073075fd49506
    Signed-off-by: Akash <akashniki@gmail.com>
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -51,7 +51,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
-    debug.sf.enable_hwc_vds=1
+    debug.sf.enable_hwc_vds=1 \
+    debug.cpurend.vsync=false
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
