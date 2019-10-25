95b11e9392af52063526ad69482ca2cf60c804b7
1582130940 <1582130940@qq.com>
2019-10-25 15:19:59 +0800

    raphael: bluetooth: Enable WBS
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -24,6 +24,8 @@
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
+#define BTM_WBS_INCLUDED   TRUE
+#define BTIF_HF_WBS_PREFERRED   TRUE
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
