ff83876cbc554a8adafd8820608d0c321e327673
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-25 01:52:13 +0000

    raphael: parts: Separate fod functions from PopupCameraService
    Change-Id: Ia19801cc97464d77f7c5fa592321cb24ed025c7c
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,6 +24,7 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.fod.FodUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -36,6 +37,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
+        FodUtils.startService(context);
         PopupCameraUtils.startService(context);
     }
 }
