20d5951a24982ddbb69456026c0ee3ef4f44f96a
TheScarastic <warabhishek@gmail.com>
2020-05-31 14:36:09 +0000

    raphael: switch to qcom bt stack
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -13,14 +13,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=0
 endif
 
-# Audio
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.a2dp_offload.supported=false \
-    persist.bluetooth.a2dp_offload.disabled=true \
-    persist.bluetooth.bluetooth_audio_hal.disabled=true \
-    persist.vendor.qcom.bluetooth.enable.splita2d=false \
-    vendor.audio.feature.a2dp_offload.enable = false
-
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=16
