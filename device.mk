a15619e429568e11734ea74861277fdd91ed8475
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    raphael: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    [SebaUbuntu]: Cleanup Android.bp and add copyright header
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -443,6 +443,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor \
