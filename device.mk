2ab4acee1d4d3ce7825b016cff6394b238397a18
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    [DNM]raphael: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b

--- device.mk
+++ device.mk
@@ -80,7 +80,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
