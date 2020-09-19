9bb7cd61dfbdb05ce35bc4e7b8d063cff3ddcec2
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    raphael: Import vendor/qcom/opensource/commonsys/packages/apps/Bluetooth soong namespace

--- device.mk
+++ device.mk
@@ -142,7 +142,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    vendor/nxp/opensource/sn100x
+    vendor/nxp/opensource/sn100x \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 
 # Telephony
 PRODUCT_PACKAGES += \
