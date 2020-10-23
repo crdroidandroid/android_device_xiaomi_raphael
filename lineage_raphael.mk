6cf5d4417f22a16887637a7c4d414be5cb533715
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-23 09:35:08 +0200

    raphael: Inherit from common tree
    Change-Id: Icaf0e5cb7dbb8c75b81bf9008433ab51a9b34531

---
+++ lineage_raphael.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from raphael device
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+PRODUCT_NAME := lineage_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 9T Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raphael-user 11 RKQ1.200826.002 V12.5.1.0.RFKMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.1.0.RFKMIXM:user/release-keys
