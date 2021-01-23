c3aa372e3eea3e06988f6c0ca377e75a488713c6
Alex <badnwz.ad@gmail.com>
2021-01-23 17:21:55 +0200

    raphael: HavocOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_raphael.mk
+    $(LOCAL_DIR)/havoc_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_raphael-user \
+    havoc_raphael-userdebug \
+    havoc_raphael-eng
