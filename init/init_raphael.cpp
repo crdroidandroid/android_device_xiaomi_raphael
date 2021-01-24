b98cb1bb02432d6a67d57598181f96925f40f005
Alex <badnwz.ad@gmail.com>
2021-01-24 08:06:39 +0200

    raphael: Update Fingerprint To Coral's January Security Patch & update build description to pass safetynet
    Signed-off-by: Alex <badnwz.ad@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -61,24 +61,21 @@ void load_raphaelglobal() {
     property_override("ro.product.model", "Mi 9T Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.1.0.PFKEUXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "coral-user 11 RQ1A.210105.003 7005429 release-keys");
 }
 
 void load_raphaelin() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphaelin");
     property_override("ro.product.device", "raphaelin");
-    property_override("ro.build.description", "raphaelin-user 9 PKQ1.181121.001 V10.3.3.0.PFKINXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "coral-user 11 RQ1A.210105.003 7005429 release-keys");
 }
 
 void load_raphael() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.12.0.PFKCNXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "coral-user 11 RQ1A.210105.003 7005429 release-keys");
 }
 
 
@@ -94,6 +91,6 @@ void vendor_load_properties() {
     } else {
         LOG(ERROR) << __func__ << ": unexcepted region!";
     }
-
+    property_override("ro.build.fingerprint", "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys");
     load_dalvik_properties();
 }
