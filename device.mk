e693fc2de226d264156f11fafd3974ae51101ac5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-25 21:31:27 +0200

    raphael: Import powerhint.json
    * From https://android.googlesource.com/device/google/coral/+/android-11.0.0_r40/powerhint.json
    
    Change-Id: Ie6946252fc832c37034d733756a7680459d191e2
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -452,6 +452,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor \
     android.hardware.power-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # PowerOffAlarm
 PRODUCT_PACKAGES += \
     PowerOffAlarm
