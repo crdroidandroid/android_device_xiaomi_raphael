b184df343bb3bf12f326254b7d9a47fc35a2abe1
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:23:32 +0000

    raphael: Setup udfps overlays
     * FOD radius = MIUI radius value / 2
     * FOD x position = MIUI x position value + (MIUI radius value / 2)
     * FOD y position = MIUI y position value + (MIUI radius value / 2)
     * FOD enroll progress radius = (MIUI radius value / 2) + 5
    
    Change-Id: Ie8add9eb4958bbb55d83d4d2809eb53b1822d15e

--- device.mk
+++ device.mk
@@ -35,7 +35,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
