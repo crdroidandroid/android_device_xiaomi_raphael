8c94a8adf6aea781ddb660ca1e350a55278e9b40
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2021-10-29 08:33:16 +0530

    raphael: Speed preopt Google dialer, Settings & SystemUI
    - Add Google dialer, Settings & SystemUI
    
    The default ART setting is quicken: run DEX code verification and
    optimize some DEX instructions to get better interpreter performance.
    
    This commit switches performance critical applications to speed:
    run DEX code verification and AOT-compile all methods.
    
    PRODUCT_DEXPREOPT_SPEED_APPS (New in Android O)
    List of applications that have been identified as core to the products
    and which are desirable to compile with the speed compiler filter.  For
    example, persistent apps such as SystemUI get a chance to use
    profile-guided compilation only at the next reboot, so it may be better
    for the product to have these apps always AOT-compiled.
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI \
+    GoogleDialer
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
