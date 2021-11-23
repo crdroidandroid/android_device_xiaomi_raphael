ce7389c42b4162af575bc2b04ed883eb9391c348
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-24 00:27:24 +0530

    raphael: Disable ART debug and optimize dexpreopt
     * don't include debug configs in art
     * use everything filter for dexpreopt (https://github.com/AOSPA/android_vendor_pa/commit/ab5061c425e61bc7d36f7bcf480feea975795545)
     * use non-debug dex2oat
    
    Change-Id: Ic310df1e2c466437b26fcb2e5ab9333694328c48
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -46,6 +46,11 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
