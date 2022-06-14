5ce04f272e4bae545718fc752e0aaa4edd956c9c
pmandri <padmashree.mandri@intel.com>
2022-06-14 18:00:12 +0530

    raphael: Update Audio HAL Interface
    Update Audio HAL from 7.0 to 7.1
    Updated files: device.mk and manifest.xml
    
    Change-Id: I7cd1d12c65604a2990d9bf80c2dea28c1a537ba5
    Tracked-On: OAM-102157
    Signed-off-by: pmandri <padmashree.mandri@intel.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
