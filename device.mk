d7483eca5b44c39cc2f709554251188b6a2edca1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-20 22:10:33 +0200

    raphael: Namespace the tree
    Change-Id: Idbec96ba811b0dff995390f85260f2d4b591f47b

--- device.mk
+++ device.mk
@@ -21,5 +21,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_SHIPPING_API_LEVEL := 28
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/raphael/raphael-vendor.mk)
