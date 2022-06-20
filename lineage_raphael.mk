fde6105a071615f4d3ed8754151460c8b28217de
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-20 22:53:51 +0200

    raphael: Update build fingerprint and description
    Change-Id: I145269c3979cd39da105ea656e089ed25891c189

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := Mi 9T Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raphael-user 11 RKQ1.200826.002 V12.5.1.0.RFKMIXM release-keys"
+    PRIVATE_BUILD_DESC="raphael-user 11 RKQ1.200826.002 V12.5.2.0.RFKMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.1.0.RFKMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
