48d652f19ff5ca0019907c395cf22e5c1c0ce2e6
Andrzej Perczak <linux@andrzejperczak.com>
2022-06-29 10:33:07 +0000

    raphael: Import lmkd props from google gki
    Reference: https://github.com/LineageOS/android_hardware_google_pixel/blob/lineage-20.0/mm/device_gki.mk
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -298,6 +298,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# LMK tuning
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.filecache_min_kb=153600 \
+    ro.lmk.kill_timeout_ms=50 \
+    ro.lmk.stall_limit_critical=40
+
 # Media
 PRODUCT_PACKAGES += \
     libarbitrarybytes \
