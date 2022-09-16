f0bd5f60769b7a9f244a6d0075c5058221f094e0
Cyber Knight <cyberknight755@gmail.com>
2022-09-16 14:56:28 +0000

    raphael: Build KProfiles - Inherit KProfiles. - Address sepolicy. - Define sysfs_kprofiles as a domain. - Fix permission of nodes relevant to KProfiles at boot.
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -274,6 +274,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.raphael
