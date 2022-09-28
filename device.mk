f90aaaa8787c758f0e55edd932d1c2c8bddca249
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:38:17 +0200

    raphael: Build default health HIDL recovery impl
    Change-Id: I9710760a1b7895ed9a0c3da967c4b30ea655bf94

--- device.mk
+++ device.mk
@@ -287,11 +287,8 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
-
-# Storage health HAL
-PRODUCT_PACKAGES += \
-    android.hardware.health.storage@1.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
