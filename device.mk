4c51c8cf00ae3c60ccbbd53be38fa51ea419d627
firebird11 <hbgassel@gmail.com>
2023-01-05 17:01:37 +0100

    raphael: Switch to common Xiaomi light
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -297,7 +297,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.raphael
+    android.hardware.light-service.xiaomi
 
 # Livedisplay
 PRODUCT_PACKAGES += \
