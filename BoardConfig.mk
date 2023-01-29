1575fd84576da98880ef70e5c5648c57ae27ccad
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-29 11:55:52 +0800

    raphael: remove legacy dt2w handling
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,9 +184,6 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 
-# Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
