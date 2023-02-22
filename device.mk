bc6dbe7874b457ed1cf324643b12416574c12da0
John Galt <johngaltfirstrun@gmail.com>
2023-02-22 12:59:43 -0500

    raphael: utilize task_profiles.json + cgroups.json
    We have full kernel support for uclamp and all additional functionality
    provided on current profiles.
    
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -458,6 +458,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
 
+# Task Profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
