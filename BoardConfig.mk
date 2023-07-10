7f133d2127d92ad1397cebf183ff0dd7f5169ec2
firebird11 <hbgassel@gmail.com>
2023-07-10 11:50:36 +0200

    raphael: Add Lineage device_framework_matrix.xml
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,8 @@ DEVICE_MATRIX_FILE := \
     $(DEVICE_PATH)/vintf/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/vintf/framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/vintf/framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 ODM_MANIFEST_FILES += \
     $(DEVICE_PATH)/vintf/manifest-qva.xml
