7872d8ef561dc88a7720b60214e138f42ce1d558
Pranav Vashi <neobuddy89@gmail.com>
2023-10-15 14:02:57 +0530

    raphael: Switch to wifi service aidl
    Change-Id: I48d5fd962f3fb228f0ec8636e26cd1bfd15e14d6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -560,7 +560,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
