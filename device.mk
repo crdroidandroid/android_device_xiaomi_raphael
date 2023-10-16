929c995395d09b543b786a173eef602171a6b33f
Pranav Vashi <neobuddy89@gmail.com>
2023-10-16 22:45:24 +0530

    raphael: Update libstdc++ package name
    Change-Id: Ia3255d7823a400bd378e40b02b87c74e40158787
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libshim_megvii \
-    libstdc++.vendor \
+    libstdc++_vendor \
     libwatermark_shim \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
