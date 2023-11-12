f8654bd0d43592c629ebaeb006e8beb5deee6cd4
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-12 19:44:29 +0100

    raphael: Move to health AIDL services
    Change-Id: Ibadf22d432791551c16d7f4e386b470574c390f8

--- device.mk
+++ device.mk
@@ -286,9 +286,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.example \
+    android.hardware.health-service.example_recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
