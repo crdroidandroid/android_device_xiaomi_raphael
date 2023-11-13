be3d4d214d954999cbc5ce0645584544ea690db2
Bruno Martins <bgcngm@gmail.com>
2023-11-13 17:41:11 +0000

    raphael: Migrate to QTI health AIDL service
    On top of example HAL from AOSP, QTI health AIDL HAL service enables
    suspend by default, the equivalent of setting ro.charger.enable_suspend
    for legacy charger.
    
    Change-Id: Id90368ac64275120aa48b7865b70398c89a8c439

--- device.mk
+++ device.mk
@@ -286,8 +286,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.example \
-    android.hardware.health-service.example_recovery
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1.vendor
