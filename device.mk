77215f7f14518520b43f0b11c57793d1e9ca815d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-01-26 17:27:35 +0100

    raphael: Use the fragment for QTI vibrator HAL
    Change-Id: Id07088526d4698af0872ab427d1b823e098e39d1
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -557,11 +557,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
-PRODUCT_PACKAGES += \
-      vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # Vulkan
 PRODUCT_PACKAGES += \
