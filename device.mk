7b18687ad5f7ef077424e46026437d810eebebcf
firebird11 <hbgassel@gmail.com>
2024-02-02 19:17:02 +0100

    raphael: Enable Quick Tap support
    Change-Id: I689e77b65fe0bebf4c5855761a3f1cf1cc3184c5

--- device.mk
+++ device.mk
@@ -450,6 +450,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product_privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml
 
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
