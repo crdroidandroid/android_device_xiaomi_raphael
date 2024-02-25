d2c61fc01b40a0a3707d9474b770ca370467d60d
onettboots <blackcocopet@gmail.com>
2024-02-25 10:24:55 +0000

    raphael: switch to Pixel Thermal
    Change-Id: Id7f808e18e7966e5a08c3a268ae924d281f1f083
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -584,6 +584,10 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.0.vendor
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.pixel \
+    thermal_symlinks
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
     $(LOCAL_PATH)/configs/etc/thermal-engine-msmnile-normal_mode.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-msmnile-normal_mode.conf
