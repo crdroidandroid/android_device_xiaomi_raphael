59070c5cf017136c5a80082ac75e8dee55047716
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    raphael: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
