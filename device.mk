408fb713ee5686e10737f79bbe3da6c9ce325265
firebird11 <hbgassel@gmail.com>
2024-08-24 11:26:57 +0200

    raphael: Update audio configuration
    from https://github.com/crdroidandroid/android_device_xiaomi_sm8150-common
    
    Change-Id: Ib20d519281d3c4e69bcd32ed726072c72fe0f19e

--- device.mk
+++ device.mk
@@ -82,18 +82,37 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
     android.hardware.audio@7.0-util \
-    android.hardware.audio.effect@7.0-impl
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
+    audio.primary.msmnile \
     audio.usb.default \
     audio.r_submix.default
 
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    libaudiopreprocessing \
     libbatterylistener \
-    libvolumelistener \
+    libbundlewrapper \
+    libcomprcapture \
+    libdownmix \
+    libdynproc \
+    libeffectproxy \
+    libexthwplugin \
+    libhdmiedid \
+    libhfp \
+    libldnhncr \
+    libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcompostprocbundle
+    libreverbwrapper \
+    libsndmonitor \
+    libspkrprot \
+    libvisualizer \
+    libvolumelistener
 
 PRODUCT_PACKAGES += \
     libaudio-resampler
@@ -107,6 +126,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
@@ -241,6 +261,7 @@ TARGET_HAS_UDFPS := true
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
+    libqcomfm_jni \
     qcom.fmradio
 
 # F2FS
