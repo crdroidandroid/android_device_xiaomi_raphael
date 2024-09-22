4ff70adf857c4394f18c1549b4cb5cc66a9a6179
kondors1995 <normandija1945@gmail.com>
2024-09-22 15:22:43 +0300

    raphael: Switch to common UDFPS HAL
    Change-Id: Ib3f1092f7eb6ac47356a48dbce054aaf8a5b6af7
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -248,8 +248,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.raphael \
-    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
+    libudfpshandler
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
---
+++ udfps/UdfpsHandler.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHandler.xiaomi_msmnile"
+
+#include "UdfpsHandler.h"
+
+#include <android-base/logging.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <thread>
+#include <unistd.h>
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_FOD 1
+#define PARAM_NIT_NONE 0
+
+static const char* kFodUiPaths[] = {
+        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui",
+        "/sys/devices/platform/soc/soc:qcom,dsi-display/fod_ui",
+};
+
+static const char* kFodStatusPaths[] = {
+        "/sys/touchpanel/fod_status",
+        "/sys/devices/virtual/touch/tp_dev/fod_status",
+};
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
+
+class XiaomiMsmnileUdfpsHandler : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t *device) {
+        mDevice = device;
+
+        std::thread([this]() {
+            int fodUiFd;
+            for (auto& path : kFodUiPaths) {
+                fodUiFd = open(path, O_RDONLY);
+                if (fodUiFd >= 0) {
+                    break;
+                }
+            }
+
+            if (fodUiFd < 0) {
+                LOG(ERROR) << "failed to open fd, err: " << fodUiFd;
+                return;
+            }
+
+            int fodStatusFd;
+            for (auto& path : kFodStatusPaths) {
+                fodStatusFd = open(path, O_WRONLY);
+                if (fodStatusFd >= 0) {
+                    break;
+                }
+            }
+
+            struct pollfd fodUiPoll = {
+                    .fd = fodUiFd,
+                    .events = POLLERR | POLLPRI,
+                    .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
+                    continue;
+                }
+
+                bool fodUi = readBool(fodUiFd);
+
+                mDevice->extCmd(mDevice, COMMAND_NIT, fodUi ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+                if (fodStatusFd >= 0) {
+                    write(fodStatusFd, fodUi ? "1" : "0", 1);
+                }
+            }
+        }).detach();
+    }
+
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        // nothing
+    }
+
+    void onFingerUp() {
+        // nothing
+    }
+
+    void preEnroll() {
+        // nothing
+    }
+    void enroll() {
+        // nothing
+    }
+    void postEnroll() {
+        // nothing
+    }
+
+    void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
+        // nothing
+    }
+
+    void cancel() {
+        // nothing
+    }
+  private:
+    fingerprint_device_t *mDevice;
+};
+
+static UdfpsHandler* create() {
+    return new XiaomiMsmnileUdfpsHandler();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
+
+extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
+    .create = create,
+    .destroy = destroy,
+};
