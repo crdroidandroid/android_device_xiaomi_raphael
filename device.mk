1354226466d511828e7581d35f2490afd3dac7df
kondors1995 <normandija1945@gmail.com>
2024-09-22 20:21:47 +0300

    raphael: Switch to AIDL UDFPS HAL
    Change-Id: If76f027293489cc104398fe3c6b241e1619e6fc6
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -248,7 +248,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     libudfpshandler
 
