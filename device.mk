c8dfbd22dfbce461866c73fd4970048c8099f182
firebird11 <hbgassel@gmail.com>
2024-09-27 08:19:50 +0200

    Revert "raphael: Remove KProfiles""
    This reverts commit 6de23bf9b5af6fa56b3ca842411ec22f8dfe3125.
    
    Change-Id: If8f162e44b64889501c23542b8cbb85f6b908294

--- device.mk
+++ device.mk
@@ -358,6 +358,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
