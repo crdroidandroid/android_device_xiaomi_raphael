65cdc2f2347a3eae9b2e0ec927a6e9e35a841a60
firebird11 <hbgassel@gmail.com>
2024-10-10 15:26:57 +0200

    Revert "raphael: Remove QTI perf"
    This reverts commit c35143ecd87c72edd566e000ed48aa04e4de6b7c.
    
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -460,6 +460,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
+    vendor.qti.hardware.perf@2.2.vendor \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
