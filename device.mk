14457b9078db54f69abe7b2ebf8f7304ca6b7d04
firebird11 <hbgassel@gmail.com>
2026-08-27 18:02:04 +0200

    raphael: Add Dolby
     * partially taken from: https://github.com/LineageOS/android_device_xiaomi_sm6150-common
    
    Change-Id: I6a12f4cc63dc9c3f8a773b0e434af00a8c32886d
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -47,6 +47,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
+# Dolby
+TARGET_USES_DOLBY := true
+$(call inherit-product, vendor/xiaomi/dolby/config.mk)
+
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Init
 $(call soong_config_set,xiaomi_msmnile,variant_lib,//$(LOCAL_PATH):libvariant_xiaomi_raphael)
 
